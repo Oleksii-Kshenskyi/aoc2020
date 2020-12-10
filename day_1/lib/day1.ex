55681a041de4ffb309d14657e98a9780e63c2156
Oleksii-Kshenskyi <oleksii.kshenskyi@outlook.com>
2020-12-10 03:28:53 +0200

    Day1, part 2 in Elixir.

--- day_1/lib/day1.ex
+++ day_1/lib/day1.ex
@@ -13,6 +13,31 @@ defmodule Day1 do
 
   """
 
+  def with_previous(current_result, previous_result), do: [previous_result, current_result]
+
+  def map_of_triple_answers(the_list) do
+    the_list
+    |> Enum.map(fn first ->
+      the_list
+      |> Enum.map(fn second ->
+        the_list
+        |> Enum.map(fn third ->
+          if first + second + third == 2020, do: first * second * third
+        end)
+      end)
+    end)
+  end
+
+  def and_also_count_triples_2020(previous_result, the_list) do
+    the_list
+    |> map_of_triple_answers
+    |> Enum.flat_map(fn elem -> elem end)
+    |> Enum.flat_map(fn elem -> elem end)
+    |> Enum.filter(fn elem -> elem != nil end)
+    |> List.first
+    |> with_previous(previous_result)
+  end
+
   def map_of_answers(the_list) do
     the_list
     |> Enum.map(fn first ->
@@ -33,11 +58,15 @@ defmodule Day1 do
 
 
   def main([filename]) do
-    File.stream!(filename)
+    the_list = File.stream!(filename)
     |> Stream.map(&String.trim/1)
     |> Stream.map(&String.to_integer/1)
     |> Enum.to_list
+
+    the_list
     |> which_pairs_product_is_2020
+    |> and_also_count_triples_2020(the_list)
     |> IO.inspect
+
   end
 end

624dfd50618c8d7efe64566354cec5157dedc2f9
Oleksii-Kshenskyi <oleksii.kshenskyi@outlook.com>
2020-12-10 18:36:08 +0200

    Day 2, part 2 in Rust ready.

--- day_2/src/main.rs
+++ day_2/src/main.rs
@@ -41,22 +41,39 @@ impl RulesAndPasses {
         RulesAndPasses { rules_and_passes: rules_and_passes }
     }
 
-    pub fn does_pass_conform_to_rule(tuple: &(Rule, Password)) -> bool {
+    pub fn does_pass_conform_to_1st_rules(tuple: &(Rule, Password)) -> bool {
         let (rule, pass) = tuple;
         let count_of_matches = pass.password.matches(rule.character).count();
 
         count_of_matches >= rule.lower_bound as usize && count_of_matches <= rule.upper_bound as usize
     }
 
-    pub fn count_conformities(&self) -> usize {
-        let mut conformities: usize = 0;
+    pub fn does_pass_conform_to_2nd_rules(tuple: &(Rule, Password)) -> bool {
+        let (rule, pass) = tuple;
+        let mut matches: u32 = 0;
+        if pass.password.chars().nth((rule.lower_bound - 1) as usize).unwrap() == rule.character {
+            matches += 1;
+        }
+        if pass.password.chars().nth((rule.upper_bound - 1) as usize).unwrap() == rule.character {
+            matches += 1;
+        }
+        
+        matches == 1
+    }
+
+    pub fn count_conformities(&self) -> (usize, usize) {
+        let mut conformities_1: usize = 0;
+        let mut conformities_2: usize = 0;
         for tuple in &self.rules_and_passes {
-            if RulesAndPasses::does_pass_conform_to_rule(tuple) {
-                conformities += 1;
+            if RulesAndPasses::does_pass_conform_to_1st_rules(tuple) {
+                conformities_1 += 1;
+            }
+            if RulesAndPasses::does_pass_conform_to_2nd_rules(tuple) {
+                conformities_2 += 1;
             }
         }
 
-        conformities
+        (conformities_1, conformities_2)
     }
 }
 
@@ -67,5 +84,8 @@ fn main() {
     let lines: Vec<String> = std::fs::read_to_string(filename).unwrap().lines().map(|elem| elem.to_owned()).collect::<Vec<String>>();
     let rap = RulesAndPasses::from_lines(lines);
     
-    println!("In the current puzzle input, {} passes confirm to rules.", rap.count_conformities());
+    let (part_1, part_2) = rap.count_conformities();
+
+    println!("{} passes confirm to rules according to the interpretation from part 1.", part_1);
+    println!("{} passes confirm to rules according to the interpretation from part 2.", part_2);
 }

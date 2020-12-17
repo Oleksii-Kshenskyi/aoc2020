ae0904c09e4a581d390cb63a5067a2a0837ab8d1
Oleksii-Kshenskyi <oleksii.kshenskyi@outlook.com>
2020-12-17 04:01:56 +0200

    Day 7, part 2 in Python.

--- day_7/day_7.py
+++ day_7/day_7.py
@@ -47,6 +47,38 @@ def who_contains_targets(rules, list_of_names):
         target_containers += who_contains_targets(rules, target_containers)
     return list(set(target_containers))
 
+
+
+def rule_by_name(rule_name, rules):
+    for rule in rules:
+        if rule.container == rule_name:
+            return rule
+    
+    return None
+
+def how_many_does_rule_contain(rule, rules):
+    number_of_contained = 0
+    for child in rule.children:
+        childs_rule = rule_by_name(child[1], rules)
+        child_contains = 0 if childs_rule.children == [] else how_many_does_rule_contain(childs_rule, rules)
+        number_of_contained += child[0] + child[0] * child_contains
+
+    return number_of_contained
+
+def which_counts_do_targets_contain(rules,targets):
+    target_counts = []
+
+    for target in targets:
+        for rule in rules:
+            if rule.container == target:
+                target_counts += [how_many_does_rule_contain(rule, rules)]
+
+    return target_counts
+
+def how_many_do_targets_contain(rules, targets):
+    return sum(which_counts_do_targets_contain(rules, targets))
+
+
 def main():
     if len(sys.argv) <= 1:
         print("ERROR: need the name of the file with puzzle input!")
@@ -55,7 +87,9 @@ def main():
         print("ERROR: first argument to the script has to be the name of the file with puzzle input.")
         return
     lines = list(file_to_lines(sys.argv[1]))
-    print("Number of bags containing 'shiny gold':", len(who_contains_targets(lines_to_rules(lines), ["shiny gold"])))
+    rules = lines_to_rules(lines)
+    print("Number of bags containing 'shiny gold':", len(who_contains_targets(rules, ["shiny gold"])))
+    print("Number of bags 'shiny gold' contains:", how_many_do_targets_contain(rules, ["shiny gold"]))
 
 
 if __name__ == "__main__":

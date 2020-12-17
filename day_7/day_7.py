bf2c05cdf743597baa69ccc0a6d5ab2003fdc1b2
Oleksii-Kshenskyi <oleksii.kshenskyi@outlook.com>
2020-12-17 02:03:28 +0200

    Day 7, part 1 in Python.

--- day_7/day_7.py
+++ day_7/day_7.py
@@ -1,11 +1,51 @@
+from dataclasses import dataclass
 import sys
 import os
 
-def file_to_group_answers(filename):
+@dataclass
+class Rule:
+    container: str
+    children: list
+
+def file_to_lines(filename):
     file_string = ""
     with open(filename, "r") as filed:
         file_string = filed.read()
     return file_string.split("\n")
+
+def line_to_rule(line):
+    rule = Rule("", [])
+    contain_split = line.split(" contain ")
+    rule.container = " ".join(contain_split[0].split(" ")[:-1])
+
+    children_unparsed = contain_split[1][:-1].split(", ")
+
+    if children_unparsed == ["no other bags"]:
+        return rule
+
+    children = []
+    for child_u in children_unparsed:
+        child_split = child_u.split(" ")
+        count = int(child_split[0])
+        bag_kind = " ".join(child_split[1:3])
+        children += [(count, bag_kind)]
+
+    rule.children = children
+    return rule
+
+def lines_to_rules(lines):
+    return list(map(lambda line: line_to_rule(line), lines))
+
+def who_contains_targets(rules, list_of_names):
+    target_containers = []
+    for rule in rules:
+        for name in list_of_names:
+            children_names = list(child[1] for child in rule.children)
+            if name in children_names:
+                target_containers += [rule.container]
+    if target_containers != []:
+        target_containers += who_contains_targets(rules, target_containers)
+    return list(set(target_containers))
 
 def main():
     if len(sys.argv) <= 1:
@@ -14,8 +54,9 @@ def main():
     elif not os.path.isfile(sys.argv[1]):
         print("ERROR: first argument to the script has to be the name of the file with puzzle input.")
         return
-    file_list = list(file_to_group_answers(sys.argv[1]))
-    print("Puzzle Input: ", file_list)
+    lines = list(file_to_lines(sys.argv[1]))
+    print("Number of bags containing 'shiny gold':", len(who_contains_targets(lines_to_rules(lines), ["shiny gold"])))
+
 
 if __name__ == "__main__":
     main()

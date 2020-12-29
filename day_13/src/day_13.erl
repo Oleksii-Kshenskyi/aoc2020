f7c265a8a37cfd60da9ad6375a948bd80e8ac989
Oleksii-Kshenskyi <oleksii.kshenskyi@outlook.com>
2020-12-29 16:49:53 +0200

    Day 13, part 2 in Erlang: boilerplate for part 2 solution ready.

--- day_13/src/day_13.erl
+++ day_13/src/day_13.erl
@@ -3,13 +3,27 @@
 %% API exports
 -export([main/1]).
 
--record(bus_crap, { earliestT = 0, busIDs = [] } ).
+-record(bus_crap_1, { earliestT = 0, busIDs = [] } ).
+-record(bus_crap_2, { busID = 0, offset = 0} ).
 
-bus_crap_from_file(Contents) -> 
+bus_crap_for_part_1_from_file(Contents) -> 
     LineByLine = binary:split(Contents, [<<"\n">>], [global, trim]),
     EarliestT = binary_to_integer(lists:nth(1, LineByLine)),
     BusIDs = lists:map(fun(Int) -> binary_to_integer(Int) end, lists:filter(fun(Str) -> Str =/= <<"x">> end, binary:split(lists:nth(2, LineByLine), [<<",">>], [global, trim]))),
-    #bus_crap{earliestT = EarliestT, busIDs = BusIDs}.
+    #bus_crap_1{earliestT = EarliestT, busIDs = BusIDs}.
+
+gen_bus_crap_2(BC2Records, _, []) -> BC2Records;
+gen_bus_crap_2(BC2Records, CurrentOffset, [IDH|IDT]) when IDH =/= <<"x">> ->
+    gen_bus_crap_2(BC2Records ++ [#bus_crap_2 { busID = binary_to_integer(IDH), offset = CurrentOffset}], CurrentOffset + 1, IDT);
+gen_bus_crap_2(BC2Records, CurrentOffset, [_|IDT]) ->
+    gen_bus_crap_2(BC2Records, CurrentOffset + 1, IDT).
+gen_bus_crap_2(RawListOfIDs) -> gen_bus_crap_2([], 0, RawListOfIDs).
+
+
+bus_crap_for_part_2_from_file(Contents) -> 
+    Unfiltered = binary:split(lists:nth(2, binary:split(Contents, <<"\n">>, [global, trim])), <<",">>, [global, trim]),
+    gen_bus_crap_2(Unfiltered).
+
 
 
 solve_part_1(EarliestT, CurrentT, _, [BIH]) when CurrentT rem BIH == 0 -> (CurrentT - EarliestT) * BIH;
@@ -18,16 +32,26 @@ solve_part_1(EarliestT, CurrentT, _, [BIH|_]) when CurrentT rem BIH == 0 -> (Cur
 solve_part_1(EarliestT, CurrentT, BusIDs, [_|BIT]) -> solve_part_1(EarliestT, CurrentT, BusIDs, BIT).
 solve_part_1(EarliestT, BusIDs) -> solve_part_1(EarliestT, EarliestT, BusIDs, BusIDs).
 
-part1_solution_from_bus_crap(Crap) -> solve_part_1(Crap#bus_crap.earliestT, Crap#bus_crap.busIDs).
+part1_solution_from_bus_crap_1(Crap) -> solve_part_1(Crap#bus_crap_1.earliestT, Crap#bus_crap_1.busIDs).
+
+part2_solution_from_bus_crap_2(Crap) ->
+    Crap.
 
 part_1(Contents) ->
-    io:format("Part 1: Earliest bus ID * minutes to wait: ~w;~n", [part1_solution_from_bus_crap(Contents)]).
+    BusCrap = bus_crap_for_part_1_from_file(Contents),
+    io:format("Part 1: Earliest bus ID * minutes to wait: ~w;~n", 
+              [part1_solution_from_bus_crap_1(BusCrap)]).
+
+part_2(Contents) ->
+    BusCrap = bus_crap_for_part_2_from_file(Contents),
+    io:format("Part 2: Earliest timestump with subsequent departures: ~p;~n",
+              [part2_solution_from_bus_crap_2(BusCrap)]).
 
 solve_file(Filename) ->
     {ok, Contents} = file:read_file(Filename),
-    BusCrap = bus_crap_from_file(Contents),
     io:format("=====~s:=====~n", [Filename]),
-    part_1(BusCrap).
+    part_1(Contents),
+    part_2(Contents).
 
 solve_all([]) -> ok;
 solve_all([Name|Tail]) -> solve_file(Name), solve_all(Tail).
